9bc83ee519b48cdd63edcbcf89c13b7055adfb39
Mirshod Aslonov <mirshodaslonov001@gmail.com>
2023-05-14 06:26:48 +0500

    userseeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,7 +11,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            SubjectSeeder::class
+            SubjectSeeder::class,
+            UserSeeder::class
         ]);
     }
 }
